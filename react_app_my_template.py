74bfa76a32c198ce1e0cfb34710c0a7add0e73b3
galenomoon <gui.galenocover@gmail.com>
2022-12-12 19:22:29 -0300

    creation proccess finished

--- react_app_my_template.py
+++ react_app_my_template.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from termcolor import colored
 import requests
 
 def create_and_editing(filename, content):
@@ -18,7 +19,7 @@ def create_react_app(react_app_name, creation_mode):
     generate_default_template()
   if creation_mode == '2':
     generate_default_template()
-  #   generate_custom_template()
+    # generate_custom_template()
   
 def generate_default_template():
   folders = ['components', 'pages', 'config', 'assets', 'routes', 'utils']
@@ -40,33 +41,72 @@ def remove_default_files():
   os.remove('src/App.test.js')
   os.remove('src/logo.svg')
 
-def create_repo(reponame):
+def create_repo(reponame, is_public=True):
   load_dotenv()
+  print('-' * 12)
+  print(colored("Creating repository on your github account", 'green'))
   url = 'https://api.github.com/user/repos'
   token = os.environ['GITHUB_TOKEN']
   headers = {'Authorization': 'Bearer ' + token, 'Accept': 'application/vnd.github+json'}
-  requests.post(url, json={'name': reponame}, headers=headers)
+  requests.post(url, json={'name': reponame, 'public' : is_public}, headers=headers)
   
-def git_init(reponame):
-  create_repo(reponame)
+def git_connection(reponame, is_public=True):
+  create_repo(reponame, is_public)  
+  print('-' * 12)
+  print(colored("Repository Created!", 'green'))
+  username = os.popen('git config user.name').read().replace("\n", "")
   os.system('git init')
   os.system('git add .')
   os.system('git commit -m "Initial commit"')
   os.system('git branch -M master')
-  os.system('git remote add origin git@github.com:galenomoon/'+ reponame +'.git')
+  os.system(f"git remote add origin git@github.com:{username}/{reponame}.git")
   os.system('git push -u origin master')
+  
 
-# ======================
+print(colored("Welcome to your app...", 'green'))
+print(colored('''
+██████╗ ███████ ██╗   ██╗  ███████ ███████ ████████ ██╗   ██ ██████╗ 
+██╔══██ ██╔════ ██║   ██║  ██╔════ ██╔════ ╚══██╔══ ██║   ██ ██╔══██╗
+██║  ██ █████╗  ██║   ██║  ███████ █████╗     ██║   ██║   ██ ██████╔╝
+██║  ██ ██╔══╝  ╚██╗ ██╔╝  ╚════██ ██╔══╝     ██║   ██║   ██ ██╔═══╝ 
+██████╔ ███████╗ ╚████╔╝   ███████ ███████╗   ██║   ╚██████╔ ██║     
+╚═════╝ ╚══════╝  ╚═══╝    ╚══════ ╚══════╝   ╚═╝    ╚═════╝╚═╝     
+''', 'cyan'))
+print(colored("Your react app generator!", 'green'))
 
-react_app_name = input('Enter the name of the react app: ')
+print('-' * 50)
+
+print('Enter the name of the', colored('react app: ', 'green'), end='')
+react_app_name = input()
+
+print('-' * 25)
+
 creation_mode = input('Enter the creation mode: \n1) Default\n2) Custom\n[1/2]: ')
+
+print('-' * 25)
+
 after_creation_mode = input('After creation: \n1) Start app\n2) Open code\n3) Both\n4) None\n[1/2/3/4]: ')
-connect_git = input('Connect to git? It will create a repo with the same name as the react app. \n[y/n]: ')
+
+print('-' * 25)
+
+create_git = input('Create a repo to git? It will create a repo with the same name as the react app. \n[y/n]: ')
+if create_git == 'y': 
+  is_public = input('Do you want to make your repository public? \n[y/n]: ')
+
+print(colored("Please wait... I'm generating your app...", 'green'))
+
+print('-' * 25)
 
 create_react_app(react_app_name + '-react', creation_mode)
 remove_default_files()
 
-if connect_git == 'y':
-  git_init(react_app_name + '-react')
+if create_git == 'y':
+  is_public = True if is_public == 'y' else False
+  git_connection(react_app_name + '-react', is_public)
+  print('-' * 25) 
+  username = os.popen('git config user.name').read().replace("\n", "")
+  print("Your repository is ready to use!")
+  print("Access your repository on:", colored(f"https://github.com/{username}/{react_app_name}-react", 'green'))
 
+print(colored("ALL DONE!", 'green'))
 after_creation(after_creation_mode)
